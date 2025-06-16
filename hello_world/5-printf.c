9268851b443c6a8545e9db83ae0c32d9832b22f4
Katherine Beringer <github@katbee.au>
2025-06-16 06:22:37 +0000

    Use printf to print a formatted string

---
+++ hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - use printf to format a complex string
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
