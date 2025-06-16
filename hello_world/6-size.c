647a418bc54bcfdf08e05e408a18677682ad83c2
Katherine Beringer <github@katbee.au>
2025-06-16 08:09:13 +0000

    Refactor size_array to use sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,17 +8,23 @@ int main(void)
 {
 /* Declare char and size variables in arrays */
 const char *types_array[5] = {"char", "int", "long int", "long long int", "float"};
-int size_array[5] = {1, 4, 4, 8, 4};
+int size_array[5] = {
+sizeof(char),
+sizeof(int),
+sizeof(long int),
+sizeof(long long int),
+sizeof(float)
+};
 
 /* Print outcome */
 for (int i = 0; i < 5; i++)
 {
 if (i == 1)
 {
-printf("Size of an %s: %d byte(s)\n", types_array[i], size_array[i]);
+printf("Size of an %s: %zu byte(s)\n", types_array[i], size_array[i]);
 }
 else
-printf("Size of a %s: %d byte(s)\n", types_array[i], size_array[i]);
+printf("Size of a %s: %zu byte(s)\n", types_array[i], size_array[i]);
 }
 
 return (0);
