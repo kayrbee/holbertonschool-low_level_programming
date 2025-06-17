065b3f240b057e2a3b14ab06b9a0fc20ed63160e
Katherine Beringer <github@katbee.au>
2025-06-17 04:33:51 +0000

    Print non-contiguous ascii characters

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print a contiguous series of ascii chars"
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
