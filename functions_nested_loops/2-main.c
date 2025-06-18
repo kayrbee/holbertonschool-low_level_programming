04052cae8f76eb2eb3f69f27cb6828efdfa7cf45
Katherine Beringer <github@katbee.au>
2025-06-18 05:32:16 +0000

    Nested for in while - draft

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point to program 2
+ * Return: 0
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
+
---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lc a-z 10 times
+ * Return: void to main
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i < 10)
+	{
+		/* Do somethingi 10x */
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
+
