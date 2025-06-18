48998bed78d0e4999d30b9f6bdce78c30f2bb1f0
Katherine Beringer <github@katbee.au>
2025-06-18 08:31:47 +0000

    Is c lowercase?

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "head.h"
+
+/**
+ * _islower - similar to islower but hand-rolled
+ * @c: is the character passed from main to be checked
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	int i;
+
+	if (c >= 98 && c <= 122)
+	{
+		i = 1;
+	} else
+	{
+		i = 0;
+	}
+	return (i);
+}
+
