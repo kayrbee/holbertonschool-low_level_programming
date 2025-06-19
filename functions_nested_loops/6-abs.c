379412cd96f89aa5ea4fe7fdfffc751228d81ed7
Katherine Beringer <github@katbee.au>
2025-06-19 04:05:25 +0000

    Use type casting to convert int to unsigned int

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an integer
+ * @int: passed from main for evaluation
+ * Return: tbc!
+ */
+int _abs(int)
+{
+	/* Define */
+	int i;
+
+	i = (unsigned int) n;
+
+	return (i);
+}
+
