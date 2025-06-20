52e27c66a032da12261e0e3c3518893ada1ff95c
Katherine Beringer <github@katbee.au>
2025-06-20 06:35:29 +0000

    Fully working draft but needs refactoring to avoid betty errors

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of a 24H day
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int hour = 0;
+	int j;
+	int k;
+	int minute = 0;
+	int n;
+	int o;
+
+	while (hour < 24)
+	{
+		minute = 0;
+		while (minute < 60)
+		{
+			if (hour < 10)
+			{
+				_putchar('0');
+				_putchar('0' + hour);
+				_putchar(':');
+			} else
+			{
+				j = (hour / 10);
+				k = (hour % 10);
+				_putchar('0' + j);
+				_putchar('0' + k);
+				_putchar(':');
+			}
+			if (minute < 10)
+			{
+				_putchar('0');
+				_putchar('0' + minute);
+				_putchar('\n');
+			}
+			else
+			{
+				n = (minute / 10);
+				o = (minute % 10);
+				_putchar('0' + n);
+				_putchar('0' + o);
+				_putchar('\n');
+			}
+			minute = minute + 1;
+		}
+		hour = hour + 1;
+	}
+}
+
