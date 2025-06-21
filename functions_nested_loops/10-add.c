11c1240c8909cf3e70e9ffc072d549ad5ce8c0df
Katherine Beringer <github@katbee.au>
2025-06-21 08:01:17 +0000

    Sum two ints

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer a, passed from main for sum with
+ * @b: integer b, also passed from main
+ * Return: int sum of a, b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = (a + b);
+
+	return (sum);
+}
+
