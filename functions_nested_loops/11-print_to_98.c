fc1c90ac8eb60a9cd0bf8b67557c7c61269c41e3
Katherine Beringer <github@katbee.au>
2025-06-21 09:30:30 +0000

    Remove unused variable

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,12 +10,9 @@ void print_to_98(int n)
 {
 	int start_value = n;
 	int stop_value = 98;
-	int print_length;
 
 	if (start_value <= stop_value)
 	{
-		print_length = ((stop_value - start_value) + 1);
-
 		while (start_value <= stop_value)
 		{
 			if (start_value == stop_value)
@@ -31,8 +28,6 @@ void print_to_98(int n)
 	}
 	else
 	{
-		print_length = (start_value - stop_value);
-
 		while (start_value >= stop_value)
 		{
 			if (start_value == stop_value)
