3526145c8977c0ae438bc703dbd84b88b0b58b69
Katherine Beringer <github@katbee.au>
2025-06-21 04:59:00 +0000

    Prepend the padding to get formatting right

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,12 +1,9 @@
 #include "main.h"
 
-void times_table(void);
-
 /**
  * times_table - prints the times table from (0..9)
  * Return: void
  */
-
 void times_table(void)
 {
 	int column = 0;
@@ -15,44 +12,47 @@ void times_table(void)
 	int res_ind_1;
 	int res_ind_0;
 
-	while (column <= 9)
+	while (row <= 9)
 	{
-		row = 0;
-		while (row <= 9)
+		column = 0;
+		while (column <= 9)
 		{
 			result = (row * column);
 			res_ind_0 = result % 10;
 			res_ind_1 = result / 10;
 
-			if (res_ind_1 != 0 && row < 9)
+			if (column != 0)
 			{
-				_putchar('0' + res_ind_1);
-				_putchar('0' + res_ind_0);
 				_putchar(',');
-				_putchar(' ');
-			}
-			else if (res_ind_1 == 0 && row < 9)
-			{
-				_putchar('0' + res_ind_0);
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
 
-			else if (res_ind_1 != 0 && row == 9)
+				if (result < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			
+			if (result >= 10)
 			{
 				_putchar('0' + res_ind_1);
 				_putchar('0' + res_ind_0);
-				_putchar('\n');
 			}
 			else
 			{
 				_putchar('0' + res_ind_0);
+			}
+
+			if (column == 9)
+			{
 				_putchar('\n');
 			}
-			row++;
+			column++;
 		}
-		column++;
+		row++;
 	}
 	_putchar('\n');
 	
