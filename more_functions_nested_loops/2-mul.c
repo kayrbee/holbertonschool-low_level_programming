5a74ec5746cd2845fadba5a84818c890ac70ecfc
Katherine Beringer <github@katbee.au>
2025-06-23 02:52:31 +0000

    Multiply two digits

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two digits
+ * @a: first integer from main for multiplication
+ * @b: second integer from main for multiplication
+ * Return: product of a,b
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+
+	return (product);
+}
+
