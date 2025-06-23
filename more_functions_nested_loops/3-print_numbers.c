c4039d437d660a78e22bd39ff410acde79333355
Katherine Beringer <github@katbee.au>
2025-06-23 03:00:50 +0000

    Print numbers from 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}
