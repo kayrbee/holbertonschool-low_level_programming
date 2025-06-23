6232c5911a8c252e5c53adfc02a1e6e3b9c45eea
Katherine Beringer <github@katbee.au>
2025-06-23 03:15:11 +0000

    Print most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 excluding 2 & 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	i = 48;
+	while (i <= 57)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
+
