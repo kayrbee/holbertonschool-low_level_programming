a522d7efe30c4084cb7ef26b8c520895a0a6dae5
Katherine Beringer <github@katbee.au>
2025-06-24 06:45:52 +0000

    Use pointers to swap two values - draft

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of a and b
+ * @a: address of var a
+ * @b: address of var b
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*new_a = b;
+	*new_b = a;
+}
+
