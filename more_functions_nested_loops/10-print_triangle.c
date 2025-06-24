930ee47967236678687ad264c12c6b6072c74e77
Katherine Beringer <github@katbee.au>
2025-06-24 03:15:35 +0000

    Play with var names for clarity

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -7,29 +7,29 @@
  */
 void print_triangle(int size)
 {
-	int row = 0;
-	int stop = size;
-	int spaces = (size - 1);
+	int current_line_number = 0;
+	int size_of_triangle = size;
+	int number_of_spaces_to_print = (size - 1);
 	int i;
 
 	if (size <= 0)
 		_putchar('\n');
 
-	while (row < stop)
+	while (current_line_number < size_of_triangle)
 	{
-		for (i = spaces; i > 0; i--)
+		for (i = number_of_spaces_to_print; i > 0; i--)
 		{
 			_putchar(' ');
 		}
 
-		for (i = spaces; i < stop; i++)
+		for (i = number_of_spaces_to_print; i < size_of_triangle; i++)
 		{
 			_putchar('#');
 		}
 
-		spaces = spaces - 1;
+		number_of_spaces_to_print--;
 		_putchar('\n');
-		row++;
+		current_line_number++;
 	}
 }
 
