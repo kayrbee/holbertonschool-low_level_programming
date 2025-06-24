89e9f41e9e3067f396341618cf0fd9647483885e
Katherine Beringer <github@katbee.au>
2025-06-24 06:33:45 +0000

    Hello, pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - uses a pointer to update the value of an int var
+ * @n: a pointer to the integer to be updated via dereferencing
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
