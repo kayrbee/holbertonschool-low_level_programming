47e9f962c57d69cc11617fd50d3757b3b47e55df
Katherine Beringer <github@katbee.au>
2025-06-25 08:40:30 +0000

    Reverse a string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - reverses a string
+ * @s: string to be reversed, provided by main
+ * Return - void
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int element;
+
+	/* find array length */
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (element = length; element >= 0; element--)
+	{
+		_putchar(s[element]);
+	}
+	_putchar('\n');
+}
+
