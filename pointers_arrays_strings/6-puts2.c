da05691cc5db25e5ba53399c30843adfb956333d
Katherine Beringer <github@katbee.au>
2025-06-26 12:34:42 +0000

    Print every second character

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character in string, starting from [0]
+ * @str: string to process, passed from main
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int e = 0;
+
+	while (e != '\0')
+	{
+		if (e % 2 == 0)
+		{
+			_putchar(str[e]);
+		}
+		e++;
+	}
+}
+
