4f7816631a5dc8adacc16620b2078b520d127795
Katherine Beringer <github@katbee.au>
2025-06-27 02:37:49 +0000

    Set up function file - wip

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strcopy - copy string to new location
+ * @dest: new location of string
+ * @src: original location of string
+ * Return: character pointer
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
