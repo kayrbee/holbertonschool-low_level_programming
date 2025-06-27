279ea0f9ce01f018a2a4a2cae99c4f8b4fb0eb57
Katherine Beringer <github@katbee.au>
2025-06-27 02:21:04 +0000

    Print n values of integer array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements in an array in order from a[0]
+ * @a: array to be read
+ * @n: number of elements to be printed
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int element = 0;
+	int elements_to_print = n;
+	int array_length;
+
+	printf("%d", a[element]);
+	element++;
+
+	while (element < elements_to_print)
+	{
+		printf(", %d", a[element]);
+		element++;
+	}
+	printf("\n");
+}
+
