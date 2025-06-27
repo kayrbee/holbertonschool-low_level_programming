fe5e752849d6b61ccd223584f68dab5b97b8b05d
Katherine Beringer <github@katbee.au>
2025-06-27 03:42:30 +0000

    Copy a string to a new home

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
- * _strcopy - copy string to new location
+ * _strcpy - copy string to new location
  * @dest: new location of string
  * @src: original location of string
  * Return: character pointer
  */
 char *_strcpy(char *dest, char *src)
 {
+	int element = 0;
+
+	while (src[element] != '\0')
+	{
+		dest[element] = src[element];
+		element++;
+	}
+
+	dest[element] = '\0';
+	return (dest);
 }
+
