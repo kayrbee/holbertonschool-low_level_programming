ec7e9a7a0ff7f7b053e7f7636b48201c1d237a0e
Katherine Beringer <github@katbee.au>
2025-06-29 06:54:03 +0000

    Remove main() from function file

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _atoi: convert a string array to an integer LITE
@@ -43,30 +42,5 @@ int _atoi(char *s)
 		sign = -1;
 	}
 	return ((final_integer * sign));
-}
-
-int main(void)
-{
-    int nb;
-
-    nb = _atoi("9");
-    printf("%d\n", nb);
-    nb = _atoi("98");
-    printf("%d\n", nb);
-    nb = _atoi("-402");
-    printf("%d\n", nb);
-    nb = _atoi("         ------++++++-----+++++--98");
-    printf("%d\n", nb);
-    nb = _atoi("214748364");
-    printf("%d\n", nb);
-    nb = _atoi("0");
-    printf("%d\n", nb);
-    nb = _atoi("Suite 402");
-    printf("%d\n", nb);
-    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
-    printf("%d\n", nb);
-/*    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
-    printf("%d\n", nb);*/
-    return (0);
 }
 
