371a07b22e5db795f34a6d7b041a078b27a69de8
Katherine Beringer <github@katbee.au>
2025-06-29 06:51:10 +0000

    Add conditional exit after first integer completed

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,34 +12,35 @@ int _atoi(char *s)
 	int final_integer = 0;
 	int sign = 1;
 	int count_sign = 0;
+	int in_progress = 0;
+	int eval_done = 0;
 
 	while (s[element] != '\0')
 	{
-		if (s[element] >= '0' && s[element] <= '9')
+		if ((s[element] >= '0') && (s[element] <= '9') && (eval_done == 0))
 		{
+			in_progress = 1;
 			final_integer = (final_integer * 10) + (s[element] - 48);
-		}
-
-		element++;
-	}
-
-	element = 0;
-	while (s[element] != '\0')
-	{
-		if (s[element] == '-')
-		{
-			count_sign++;
-		}
-
-		if (count_sign % 2 != 0)
-		{
-			sign = -1;
 		}
 		else
 		{
-			sign = 1;
+			if (in_progress == 1)
+			{
+				eval_done = 1;
+			}
 		}
+		
+		if (s[element] == '-' && in_progress == 0)
+		{
+			count_sign++;
+		}
+	
 		element++;
+	}
+
+	if (count_sign % 2 != 0)
+	{
+		sign = -1;
 	}
 	return ((final_integer * sign));
 }
@@ -54,8 +55,8 @@ int main(void)
     printf("%d\n", nb);
     nb = _atoi("-402");
     printf("%d\n", nb);
-    nb = _atoi("          ------++++++-----+++++--98");
-/*    printf("%d\n", nb);
+    nb = _atoi("         ------++++++-----+++++--98");
+    printf("%d\n", nb);
     nb = _atoi("214748364");
     printf("%d\n", nb);
     nb = _atoi("0");
@@ -64,7 +65,7 @@ int main(void)
     printf("%d\n", nb);
     nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
     printf("%d\n", nb);
-    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+/*    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
     printf("%d\n", nb);*/
     return (0);
 }
