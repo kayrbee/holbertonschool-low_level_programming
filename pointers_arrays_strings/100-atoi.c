8a33914bb99973eb257386dd0d00949a85c28a4d
Katherine Beringer <github@katbee.au>
2025-06-29 05:27:20 +0000

    Add main() for testing, add return statement

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,14 +8,41 @@
 int _atoi(char *s)
 {
 	int element = 0;
+	int final_integer = 0;
 
 	while (element != '\0')
 	{
 		if (element >= '0' && element <= '9')
 		{
-			final_digit = s[element];
+			final_integer = s[element];
 		}
 		element++;
 	}
+	return (final_integer);
+}
+
+int main(void)
+{
+    int nb;
+
+    nb = _atoi("9");
+    printf("%d\n", nb);
+/*    nb = _atoi("98");
+    printf("%d\n", nb);
+    nb = _atoi("-402");
+    printf("%d\n", nb);
+    nb = _atoi("          ------++++++-----+++++--98");
+    printf("%d\n", nb);
+    nb = _atoi("214748364");
+    printf("%d\n", nb);
+    nb = _atoi("0");
+    printf("%d\n", nb);
+    nb = _atoi("Suite 402");
+    printf("%d\n", nb);
+    nb = _atoi("         +      +    -    -98 Battery Street; San Francisco, CA 94111 - USA             ");
+    printf("%d\n", nb);
+    nb = _atoi("---++++ -++ Sui - te -   402 #cisfun :)");
+    printf("%d\n", nb);*/
+    return (0);
 }
 
