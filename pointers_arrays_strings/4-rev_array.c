658a95db502c158199c99d25c33965cabe9a612f
Katherine Beringer <github@katbee.au>
2025-06-30 04:49:31 +0000

    Reverse an int array of known length

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * reverse_array - print an integer array in reverse
+ * @a: integer array, passed from main
+ * @n: number of elements in array @a
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+	int first_element = 0;
+	int mirror_element = n - 1;
+	int temp;
+
+	if (n % 2 == 0)
+	{
+		while (first_element <= (n / 2))
+		{
+			temp = a[first_element];
+			a[first_element] = a[mirror_element];
+			a[mirror_element] = temp;
+			first_element++;
+			mirror_element--;
+		}
+	}
+	else
+	{
+		while (first_element <= ((n + 1) / 2))
+		{
+			temp = a[first_element];
+			a[first_element] = a[mirror_element];
+			a[mirror_element] = temp;
+			first_element++;
+			mirror_element--;
+		}
+	}
+}
+
