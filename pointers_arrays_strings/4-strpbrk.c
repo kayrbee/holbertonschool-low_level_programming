cf0455b7c8bfa7bec3edc7387c9661d26fb3de03
Katherine Beringer <github@katbee.au>
2025-07-01 06:19:53 +0000

    Locate a character in a string

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * _strpbrk - locate the first occurrence of a reference char in string
+ * @s: string to evaluate, from main
+ * @accept: reference string
+ * Return: pointer to first matching char, or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int s_elmt = 0;
+	int ref_elmt = 0;
+	int match = 0;
+	char *result;
+
+	while (s[s_elmt] != '\0')
+	{
+		while (accept[ref_elmt] != '\0' && match == 0)
+		{
+			if (s[s_elmt] == accept[ref_elmt])
+			{
+				match = 1;
+			}
+			ref_elmt++;
+		}
+		if (match == 1)
+		{
+			result = &s[s_elmt];
+			return (result);
+		}
+		s_elmt++;
+		ref_elmt = 0;
+	}
+
+	return (NULL);
+}
+
