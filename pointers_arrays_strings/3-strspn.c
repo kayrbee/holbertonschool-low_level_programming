26dea26614b63b69135e29f497357c77196712d6
Katherine Beringer <github@katbee.au>
2025-07-01 02:26:15 +0000

    Count number of matched characters in string

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - count positive char matches in string from a given set of chars
+ * @s: string to evaluate
+ * @accept: set of reference chars
+ * Return: number of matches in string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int element = 0;
+	int reference = 0;
+	unsigned int counter = 0;
+
+	while (s[element] != 0)
+	{
+		while (accept[reference] != 0)
+		{
+			if (s[element] == accept[reference])
+			{
+				counter++;
+			}
+			reference++;
+		}
+		reference = 0;
+		element++;
+	}
+	return (counter);
+}
+
