c803b4278dcc5a5d16404801cffac3ecdb67c700
Katherine Beringer <github@katbee.au>
2025-07-02 05:37:54 +0000

    Handle case where c is '\0')

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,6 +4,16 @@
 char *_strchr(char *s, char c)
 {
 	int e = 0;
+	int length = 0;
+
+	if (c == '\0')
+	{
+		while (s[e] != '\0')
+		{
+			e++;
+		}
+		e = e - 1;
+		return (&s[e]);
 
 	while (s[e] != '\0')
 	{
