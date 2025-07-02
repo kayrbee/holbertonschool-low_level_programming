341b4991f0a0e13e5261d77bd31384ce0f3ac8dc
Katherine Beringer <github@katbee.au>
2025-07-02 05:41:45 +0000

    Add comments

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,15 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * _strchr - locate first char c in string s
+ * @s: string to be searched
+ * @c: character to find
+ * Return: pointer to found character or NULL if char not found
+ */
 char *_strchr(char *s, char c)
 {
 	int e = 0;
-	int length = 0;
 
 	if (c == '\0')
 	{
@@ -14,7 +19,9 @@ char *_strchr(char *s, char c)
 		}
 		e = e - 1;
 		return (&s[e]);
+	}
 
+	e = 0;
 	while (s[e] != '\0')
 	{
 		if (s[e] == c)
