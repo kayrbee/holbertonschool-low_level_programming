d402538e53fac1e50f6394d9722ca30aeca0ba66
Katherine Beringer <github@katbee.au>
2025-07-03 04:15:00 +0000

    Add else block

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - put chars in a string using recursion
@@ -9,14 +10,12 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		_putchar(*s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
-}
-
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
+	else
+	{
+		putchar('\n');
+	}
 }
 
