850ca81a21629c5bb7f7372d0316ac2a1bf92ac0
Katherine Beringer <github@katbee.au>
2025-07-04 06:56:21 +0000

    Find a prime

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,7 +1,47 @@
 #include "main.h"
 
 /**
- *  -
- *  @
- *  Return:
- * /
+ * is_prime_number - checks if given number is prime
+ *  @n: number passed from main to be checked for prime
+ *  Return: 1 if prime, 0 if not prime
+ */
+int is_prime_number(int n)
+{
+	int number = n;
+	int max = ((n - 1) / 2);
+
+	if (number % 2 == 0)
+	{
+		return (1);
+	}
+
+	return (prime_helper(number, max));
+}
+/**
+ * prime_helper - uses recursion to determine if number is prime
+ * @number: number to check, passed from is_prime_number
+ * @max: maximum possible number that n could be divisible by
+ *       nb: max is rounded down to the nearest odd
+ * Return: 1 if prime, 0 if not prime
+ */
+int prime_helper(int number, int max)
+{
+	if (max % 2 == 0)
+	{
+		max = (max - 1);
+	}
+
+	if ((number % max) == 0)
+	{
+		return (0);
+	}
+	else if (max == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (prime_helper(number, max - 2));
+	}
+
+}
