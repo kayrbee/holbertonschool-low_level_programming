1b9c9f1e55b1e8c6e523c0088ba3e0d7bd17fd5c
Katherine Beringer <github@katbee.au>
2025-07-06 04:21:03 +0000

    Print the number of args passed in, not including argv[0]

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - define a program that prints nr args passed
+ * @argc: count number of arguments passed to main
+ * @argv: value of arguments passed to main
+ * Return: 0 if successful, !0 if failure
+ */
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		printf("%d\n", (argc - 1));
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
