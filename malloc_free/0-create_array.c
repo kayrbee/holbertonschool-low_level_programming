560ffe1916a99f2926d37ad7f5308afb4bfbda43
Katherine Beringer <github@katbee.au>
2025-07-07 03:16:34 +0000

    Use unsigned int & fix missing semicolon

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *ptr;
-	int i = 0;
+	unsigned int i = 0;
 
 	if (size == 0)
 		return (NULL);
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	if (ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
 	return (ptr);
