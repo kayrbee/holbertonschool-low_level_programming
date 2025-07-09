cce0e3998f16b9c77f4c21ccf37a5fd7a72daf05
Katherine Beringer <github@katbee.au>
2025-07-09 00:51:11 +0000

    concat two strings with malloc

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,63 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen - measure the length of given string
+ * @s: string to measure
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+/**
+ * string_nconcat - partially concat 2 strings
+ * @s1: string 1 should be copied in entirety
+ * @s2: string 2 should be partially appended to s1, where
+ * @n: n is the number of characters to include from s2
+ * Return: pointer to concat result or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	int len_s1 = _strlen(s1);
+	int len_s2 = _strlen(s2);
+	int i = 0;
+	int j = 0;
+
+	if (n > len_s2)
+	{
+		n = len_s2;
+	}
+	ptr = malloc(len_s1 + (n + 1));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	while (i < len_s1)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (j < n)
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
+}
+
