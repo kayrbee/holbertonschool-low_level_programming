36b034c9b4f6493df52ab6bf6dc297c72cbd91b3
Katherine Beringer <github@katbee.au>
2025-07-10 07:01:33 +0000

    Make betty happy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -20,18 +20,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (doggorego == NULL)
 	{
 		return (NULL);
-	}	
+	}
 	cpy_name = malloc(strlen(name) + 1);
 
 	if (cpy_name == NULL)
 	{
 		return (NULL);
 	}
-	
+
 	strcpy(cpy_name, name);
 
 	cpy_owner = malloc(strlen(owner) + 1);
-	
+
 	if (cpy_owner == NULL)
 	{
 		return (NULL);
