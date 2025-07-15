01dc77fd9fe22b50dd5cabe95f75f35ba267bd46
Katherine Beringer <github@katbee.au>
2025-07-15 04:18:00 +0000

    Now with docs

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 
+/**
+ * print_name - function pointer
+ * @name: name to print
+ * @f: function pointer to function to call
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL)
