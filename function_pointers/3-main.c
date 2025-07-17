cc40eb77e82ab6fe64c6817b8842618094f15997
Katherine Beringer <github@katbee.au>
2025-07-17 05:07:46 +0000

    Add function pointer calls and handle errors

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -4,26 +4,44 @@
 
 /**
  * main - main function of operator
- * Return: 0 
+ * @argc: number of arguments provided at run time
+ * @argv: array of arguments provided at run time
+ * Return: 0
  */
 int main(int argc, char **argv)
 {
 	int a, b;
 	char *op;
+	int operator;
+	int result = 0;
+
+	operator = get_op_function(argv[2]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
+	}
+
+	if (operator == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
 	}
 
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
-	op = argv[2];
 
-	/* If integer returned, print result */
+	result = operator(a, b);
 
-	/* If error returned, print error and exit with right code */
+	printf("%d\n", result);
+
 	return (0);
 }
 
