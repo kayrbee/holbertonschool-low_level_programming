8d8331f0a108d4f59d47a3d9e0044797751f593f
Katherine Beringer <github@katbee.au>
2025-07-18 04:22:53 +0000

    V1 - print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings - prints a list of strings
+ * @separator: desired separator between strings
+ * @n: size of list of strings
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strings;
+	unsigned int i = 0;
+
+	va_start(strings, n);
+	printf("%s", va_arg(strings, char *));
+	for (i = 1; i < n; i ++)
+	{
+		if (separator != NULL)
+		{
+			printf("%s%s", separator, va_arg(strings, char *));
+	
+		}
+		else
+		{
+			printf("%s", va_arg(strings, char *));
+		}
+	}
+	printf("\n");
+	va_end(strings);
+}
+
