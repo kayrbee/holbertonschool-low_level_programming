b8e4b413d890824f526300e2de2b7d7654802ba1
Katherine Beringer <github@katbee.au>
2025-07-18 03:54:42 +0000

    Print variable-length list of numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print a variable-length list of numbers with a separator
+ * @separator: specifies the separator to use
+ * @n: size of list
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list numbers;
+	unsigned int i = 0;
+
+	va_start(numbers, n);
+	printf("%d", va_arg(numbers, int));
+	for (i = 1; i < n; i++)
+	{
+		printf("%s%d", separator, va_arg(numbers, int));
+	}
+	printf("\n");
+	va_end(numbers);
+}
+
