fa8e6f12b480e0802fba494937c9d2793b21f73c
Katherine Beringer <github@katbee.au>
2025-07-21 01:52:34 +0000

    Implement functions, fn ptr, and lookup logic

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,28 +3,94 @@
 #include <stdio.h>
 
 /**
- * TO DO: implement a struct for valid format specifiers
- *        Use a key-value pair to look up the format function to use
+ * print_all - takes a variable number of inputs
+ *             and looks up the right function for each input
+ * @format: single char key indicating desired format specifier
+ * Return: void
  */
 void print_all(const char * const format, ...)
 {
 	va_list item;
-	int i = 0;
+	int i = 0, j = 0;
+	int printed_one = 0;
+	lookup_t lookup[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str}
+	};
 
 	va_start(item, format);
-	while (format[i] != '\0')
+	while (format != NULL && format[i] != '\0')
 	{
-		if (format[i] == 's')
-			printf("String: %s\n", va_arg(item, char *));
-		if (format[i] == 'c')
-			printf("Char: %c\n", va_arg(item, int));
+		while (j < 4)
+		{
+			if (*lookup[j].key == format[i])
+			{
+				if (printed_one == 1)
+					printf(", ");
+
+				lookup[j].f(&item);
+				printed_one = 1;
+			}
+			j++;
+		}
+		j = 0;
 		i++;
 	}
 	va_end(item);
+	printf("\n");
 }
-int main(void)
+/**
+ * print_char - prints input in char format
+ * @item: input to print
+ * Return: void
+ */
+void print_char(va_list *item)
 {
-    print_all("ssci", "stSchool", "second", 'c', 4);
-    return (0);
+	int c;
+
+	c = va_arg(*item, int);
+
+	printf("%c", c);
+}
+/**
+ * print_int - prints input in integer format
+ * @item: input to print
+ * Return: void
+ */
+void print_int(va_list *item)
+{
+	int i;
+
+	i = va_arg(*item, int);
+
+	printf("%d", i);
+}
+/**
+ * print_float - prints input in float format
+ * @item: input to print
+ * Return: void
+ */
+void print_float(va_list *item)
+{
+	float f;
+
+	f = va_arg(*item, double);
+
+	printf("%f", f);
+}
+/**
+ * print_str - prints input in string format
+ * @item: input to print
+ * Return: void
+ */
+void print_str(va_list *item)
+{
+	char *str;
+
+	str = va_arg(*item, char *);
+
+	printf("%s", str ? str : "(nil)");
 }
 
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,28 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+typedef void (*format_ptr)(va_list *);
+/**
+ * struct lookup - key:value struct to match a string to a function
+ *
+ * @key: matcher
+ * @f: function pointer to matched format specifier
+ */
+typedef struct lookup
+{
+	const char *key;
+	format_ptr f;
+} lookup_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+void print_char(va_list *item);
+void print_int(va_list *item);
+void print_float(va_list *item);
+void print_str(va_list *item);
 
 #endif
