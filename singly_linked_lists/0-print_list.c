c027f4a7298238f69ba7061abf09878b95ba6434
Katherine Beringer <github@katbee.au>
2025-07-29 00:17:48 +0000

    Set up singly linked lists folder

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <sdtio.h>
+/*
+ * print_list - prints all elements in a list
+ * list_t - singly linked list to print
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+}
---
+++ singly_linked_lists/REAME.md
@@ -0,0 +1,1 @@
+A place for the singly linked list exercises
