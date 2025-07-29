6f6b2f789cd07874d7e8ee43fa4c7dc7816e3245
Katherine Beringer <github@katbee.au>
2025-07-29 00:56:13 +0000

    Define print_list function

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,10 +1,30 @@
 #include "main.h"
-#include <sdtio.h>
-/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
  * print_list - prints all elements in a list
- * list_t - singly linked list to print
+ * @h:  singly linked list to print
  * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
+	const list_t *current = h;
+	int counter = 0;
+
+	while (current != NULL)
+	{
+		if (current->str != NULL)
+		{
+			printf("[%d] %s\n", current->len, current->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+		counter++;
+		current = current->next;
+	}
+	return (counter);
 }
+
--- singly_linked_lists/main.h
+++ singly_linked_lists/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,9 +12,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
