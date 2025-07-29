f7b4b046478627eb8e670aa7ae451c55a4b72fed
Katherine Beringer <github@katbee.au>
2025-07-29 03:30:30 +0000

    Add basic logic to add_node

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * add_node - adds a node to start of list
+ * @head: current position of head node
+ * @str: value of node
+ * Return: new head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	char *s = strdup(str);
+	list_t *new_node = (list_t *)malloc(sizeof(new_node));
+
+	new_node->str = s;
+	new_node->len = strlen(s);
+	new_node->next = *head;
+
+	*head = new_node;
+	return (*head);
+}
+
