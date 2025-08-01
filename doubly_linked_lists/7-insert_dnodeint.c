6ae1aaf47dab79123c3b416452183a7667209e00
Katherine Beringer <github@katbee.au>
2025-08-01 02:46:37 +0000

    Fix/handle out of bounds

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -46,6 +46,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		/* If list not empty AND idx > 0, look up indexed list item */
 		insert_at = get_dnodeint_at_index(insert_at, (idx - 1));
+		if (insert_at == NULL)
+			return (NULL);
 	}
 
 	/* Create new node */
