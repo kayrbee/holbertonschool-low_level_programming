b25b11ea507e1e9a6d61c5178c929e8f2bdb6f2e
Katherine Beringer <github@katbee.au>
2025-08-01 03:01:24 +0000

    Set up 8 file

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * delete_dnodeint_at_index - delete a node from a list
+ * @head: pointer to start of list
+ * @index: node to be deleted
+ * Return: 1 on success, -1 on fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+}
+
