afb78d5383c60c2c847d36b3a7e92d1f276fcac8
Katherine Beringer <github@katbee.au>
2025-08-14 05:10:24 +0000

    Get fork working 5 times

--- shell-tests/6-fork-wait-execve.c
+++ shell-tests/6-fork-wait-execve.c
@@ -2,32 +2,19 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-int main(void)
+#include <stdlib.h>
+
+int main()
 {
-	/*
-	 * Write a program that executes the command ls -l /tmp in 5 different child processes. 
-	 * Each child should be created by the same process (the father). 
-	 * Wait for a child to exit before creating a new child.
-	 */
-
-	pid_t parent_pid;
-	pid_t child_pid;
-	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
-	int status;
-
-	child_pid = fork();
-	parent_pid = getpid();
-	if (child_pid == -1)
-		perror("Something went wrong!");
-	if (child_pid == 0)
-	{
-		printf("Child - My pid: %u\n", parent_pid);
-		execve(argv[0], argv, NULL);
-	}
-	else
-	{
-		wait(&status);
-		printf("Parent %u of child  %u\n", parent_pid, child_pid);
-	}
-	return (0);
+    for(int i = 0; i < 5; i++)
+    {
+        if(fork() == 0)
+        {
+            printf("[son] pid %d from [parent] pid %d\n",getpid(),getppid());
+            exit(0);
+        }
+    }
+    for(int i = 0; i < 5; i++)
+    wait(NULL);
+    
 }
