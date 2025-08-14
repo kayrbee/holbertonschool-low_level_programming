8650a1a21f42531f2b1807af553d9e159d1fad29
Katherine Beringer <github@katbee.au>
2025-08-14 05:29:41 +0000

    Add execve logic

--- shell-tests/6-fork-wait-execve.c
+++ shell-tests/6-fork-wait-execve.c
@@ -7,20 +7,24 @@
 int main()
 {
 	int status, i = 0;
+	char *argv[] = {"/bin/ls", "-l", "/tmp", NULL};
 
 	while (i < 5)
 	{
 		if(fork() == 0)
 		{
-			printf("[child] pid %d from [parent] pid %d\n",getpid(),getppid());
+			printf("child %d of parent %d\n", getpid(), getppid());
+			execve(argv[0], argv, NULL);
 			exit(0);
 		}
+		wait(&status);
+		printf("%d\n", i);
 		i++;
 	}
 	i = 0;
-	while (i < 5)
+/*	while (i < 5)
 	{
 		wait(&status);
 		i++;
-	}
+	} */
 }
