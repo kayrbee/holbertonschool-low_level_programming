ebd42e8bfec003de420fb10cd0a860af2bc639b7
Katherine Beringer <github@katbee.au>
2025-08-15 02:50:17 +0000

    Shell still works if envp[] = {NULL}

---
+++ shell-tests/7a-super_simple_shell_experiment.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+//extern char **environ;
+
+int main()
+{
+	char *line;
+	size_t buffer_size = 0;
+	ssize_t num_chars_read;
+	int child;
+
+	child = fork();
+
+	if (child == -1)
+	{
+		perror("Fork you\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (child == 0)
+	{
+		printf("$: ");
+		num_chars_read = getline(&line, &buffer_size, stdin);
+		line[strcspn(line, "\n")] = '\0'; // trim trailing newline
+		char *argv[] = {line, NULL};
+		char *envp[] = {NULL};
+		if (execve(argv[0], argv, envp) == -1)
+		{
+			perror("execve failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(NULL);
+		return (0);
+	}
+}
