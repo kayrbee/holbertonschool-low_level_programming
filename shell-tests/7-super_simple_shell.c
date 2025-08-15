8db20b7405e8174f70c519097b54f2538152aa3e
Katherine Beringer <github@katbee.au>
2025-08-15 02:57:49 +0000

    Remove unnecessary variable

--- shell-tests/7-super_simple_shell.c
+++ shell-tests/7-super_simple_shell.c
@@ -3,13 +3,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <string.h>
-extern char **environ;
+extern char **environ; // captures external env vars
 
 int main()
 {
-	char *line;
-	size_t buffer_size = 0;
-	ssize_t num_chars_read;
+	char *line;		//Feeds getline buffer, stores user input
+	size_t buffer_size = 0;	//Holds allocated size of getline buffer
 	int child;
 
 	child = fork();
@@ -22,7 +21,7 @@ int main()
 	else if (child == 0)
 	{
 		printf("$: ");
-		num_chars_read = getline(&line, &buffer_size, stdin);
+		getline(&line, &buffer_size, stdin);
 		line[strcspn(line, "\n")] = '\0'; // trim trailing newline
 		char *argv[] = {line, NULL};
 		if (execve(argv[0], argv, environ) == -1)
